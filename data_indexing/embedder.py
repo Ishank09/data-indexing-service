a2dd2787d11e7b0a30a9cd4c0a86b33e6582f5cd
Ishank Vasania <ivasania@Ishanks-MacBook-Pro.local>
2025-07-17 00:38:44 -0700

    vector storage

--- data_indexing/embedder.py
+++ data_indexing/embedder.py
@@ -1,6 +1,7 @@
 from FlagEmbedding import FlagModel
 
 from data_indexing import utils
+from tqdm import tqdm
 
 def load_embedder():
     model_name = utils.get_env_var("EMBEDDER_MODEL_NAME")
@@ -9,7 +10,7 @@ def load_embedder():
 
 def embed_chunks(chunk_records: list[dict]):
     model = load_embedder()
-    for chunk in chunk_records:
+    for chunk in tqdm(chunk_records, desc="Embedding chunks"):
         text = chunk["chunk_text"]
         embedding = model.encode(text)
         chunk["embedding"] = embedding.tolist()
